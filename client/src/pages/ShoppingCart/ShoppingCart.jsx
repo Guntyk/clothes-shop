dbb64b2aa4e9b6e7bca8aa051676c50729431577
Daniil <mashyn.daniil@gmail.com>
2024-06-21 14:30:38 +0300

    Console.log remove

--- client/src/pages/ShoppingCart/ShoppingCart.jsx
+++ client/src/pages/ShoppingCart/ShoppingCart.jsx
@@ -103,7 +103,7 @@ export default function ShoppingCart() {
       };
 
       const result = await createNewOrder(formedOrder);
-      console.log(result);
+
       if (result) {
         setName('');
         setPhone('');
